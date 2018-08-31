aa54db92d310be0d1f4d86331073ef65f83e17d7
Fernando Pablo <ferpabeng@gmail.com>
2018-08-31 17:11:27 +0200

    Initial commit

---
+++ src/main/java/es/fpablo/console/config/WebConfiguration.java
@@ -0,0 +1,31 @@
+package es.fpablo.console.config;
+
+import javax.sql.DataSource;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+public class WebConfiguration {
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+    
+    @Bean
+	@Profile("default")
+	public DataSource embeddedDatasource() {
+		return new EmbeddedDatabaseBuilder()
+				.setName("testFer")
+				.setType(EmbeddedDatabaseType.H2)
+				.addScript("classpath:schema2018.sql")
+				.build();
+	}
+}
